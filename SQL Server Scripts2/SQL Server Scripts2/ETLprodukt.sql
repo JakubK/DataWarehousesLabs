47a41df93a7b24d62066bc69cb85011117786914
LukaszKond <potrix3@gmail.com>
2021-12-11 20:26:24 +0100

    added ETL for product

--- SQL Server Scripts2/SQL Server Scripts2/ETLprodukt.sql
+++ SQL Server Scripts2/SQL Server Scripts2/ETLprodukt.sql
@@ -3,8 +3,23 @@
 USE CALLCENTER
 GO
 
+If (object_id('dbo.ProducentTemp') is not null) DROP TABLE dbo.ProducentTemp;
+CREATE TABLE ProducentTemp(Id int, Nazwa varchar(100));
+GO
+
+BULK INSERT ProducentTemp
+	FROM 'C:\dev\HDLab\DataWarehousesLabs\DataGenerator\t0_producers.csv'
+	WITH
+	(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n',
+		TABLOCK
+	)
+
+
 If (object_id('dbo.ProduktTemp') is not null) DROP TABLE dbo.ProduktTemp;
-CREATE TABLE ProduktTemp(Id int, Nazwa varchar(100));
+CREATE TABLE ProduktTemp(Id int, Nazwa varchar(100), Cena decimal, Marza decimal, [Id_Producent] int);
 GO
 
 BULK INSERT ProduktTemp
@@ -16,10 +31,16 @@ BULK INSERT ProduktTemp
 		ROWTERMINATOR = '\n',
 		TABLOCK
 	)
+	SELECT [Id_Producent] FROM ProduktTemp
 
-MERGE Producent t USING ProduktTemp s
+
+MERGE Produkt t USING ProduktTemp s
 	ON t.Nazwa = s.Nazwa
-	WHEN NOT MATCHED BY TARGET
-		THEN INSERT (Nazwa) VALUES(s.Nazwa);
+	WHEN NOT MATCHED BY TARGET THEN
+		INSERT (Nazwa, Id_Producent) VALUES(s.Nazwa, 
+		(SELECT p.Id 
+		FROM Producent p JOIN ProducentTemp tmp ON p.Nazwa = tmp.Nazwa
+		WHERE s.Id_Producent = tmp.Id));
+
 
 DROP TABLE ProduktTemp

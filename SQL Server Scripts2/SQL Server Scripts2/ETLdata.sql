8a30a59ae43c6bb93aa4e2bf70db41d991459999
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-12-11 14:42:32 +0100

    Set Polish for month names and improve the loop

--- SQL Server Scripts2/SQL Server Scripts2/ETLdata.sql
+++ SQL Server Scripts2/SQL Server Scripts2/ETLdata.sql
@@ -5,19 +5,22 @@ DECLARE @DayInProcess int = 0;
 DECLARE @UpperYearBound int = 2030;
 
 
+SET LANGUAGE Polish
+
 WHILE @YearInProcess <= @UpperYearBound
-BEGIN
-	WHILE @MonthInProcess <= 12
-		BEGIN
-			DECLARE @tmpDate datetime = datefromparts(@YearInProcess, @MonthInProcess, 1)
-			WHILE @DayInProcess <= day(eomonth (@tmpDate))
-				BEGIN
-					INSERT INTO Data(Rok, Miesiac, [Nazwa Miesiaca], Dzien)
-					VALUES(@YearInProcess, @MonthInProcess, DateName( month , DateAdd( month , @MonthInProcess , -1 )), @DayInProcess)
-					SET @DayInProcess = @DayInProcess + 1
-				END
-			SET @MonthInProcess = @MonthInProcess + 1
-		END
-	SET @YearInProcess = @YearInProcess + 1
-END
+	BEGIN
+		WHILE @MonthInProcess <= 12
+			BEGIN
+				DECLARE @tmpDate datetime = datefromparts(@YearInProcess, @MonthInProcess, 1)
+				WHILE @DayInProcess <= day(eomonth (@tmpDate))
+					BEGIN
+						INSERT INTO Data(Rok, Miesiac, [Nazwa Miesiaca], Dzien)
+						VALUES(@YearInProcess, @MonthInProcess, DateName( month , DateAdd( month , @MonthInProcess , -1 )), @DayInProcess)
+						SET @DayInProcess = @DayInProcess + 1
+					END
+				SET @MonthInProcess = @MonthInProcess + 1
+				SET @DayInProcess = 0
+			END
+		SET @YearInProcess = @YearInProcess + 1
+	END
 GO

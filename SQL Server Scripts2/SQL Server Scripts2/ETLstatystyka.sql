6ed94737361731e0e563703bd9c0c8b02b546d72
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-12-13 12:26:11 +0100

    Add incomplete stat ETL

--- SQL Server Scripts2/SQL Server Scripts2/ETLstatystyka.sql
+++ SQL Server Scripts2/SQL Server Scripts2/ETLstatystyka.sql
@@ -1,39 +1,36 @@
-USE CALLCENTER
-GO
 
-If (object_id('dbo.statTemp') is not null) DROP TABLE dbo.statTemp;
-CREATE TABLE statTemp(Year int,Month int,AgentId int ,FirstName NVARCHAR,LastName NVARCHAR,DepartmentId int,HourlyRate decimal,Bonus decimal,HourCount int,Salary decimal);
-GO
+	USE CALLCENTER
+	GO
 
-BULK INSERT dbo.statTemp
-	FROM 'C:\dev\HDLab\DataWarehousesLabs\DataGenerator\t0_excel.csv'
-	WITH
-	(
-		FIRSTROW = 2,
-		FIELDTERMINATOR = '|',
-		ROWTERMINATOR = '\n',
-		TABLOCK
-	)
+	If (object_id('dbo.statTemp') is not null) DROP TABLE dbo.statTemp;
+	CREATE TABLE statTemp(Year int,[Month] TEXT,AgentId int ,FirstName TEXT,LastName TEXT,DepartmentId int,HourlyRate decimal,Bonus decimal,HourCount int,Salary decimal);
+	GO
+	BULK INSERT dbo.statTemp
+		FROM 'D:\DataWarehousesLabs\DataGenerator\t0_excel.csv'
+		WITH
+		(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = '|',
+			ROWTERMINATOR = '\n',
+			TABLOCK
+		)
 
 If (object_id('agentIds') is not null) DROP VIEW agentIds;
 GO
 CREATE VIEW agentIds
 AS
-SELECT Id, [Imie i Nazwisko] FROM dbo.Agent;
+SELECT Id, [Imie i Nazwisko], Month, Year, Bonus, HourCount, Salary, HourlyRate FROM dbo.Agent
+INNER JOIN dbo.statTemp ON dbo.Agent.[Imie i Nazwisko] = CAST(dbo.statTemp.[FirstName] as VARCHAR) + ' ' + CAST(dbo.statTemp.[LastName] as VARCHAR);
 GO
 
-MERGE StatystykaPracownicza t USING dbo.statTemp s
-	ON agentIds.[Imie i Nazwisko] = dbo.statTemp.[FirstName] + ' ' + dbo.statTemp.[LastName]
-	AND t.Id_Data = (SELECT Id FROM dbo.Data WHERE Rok = s.Year AND Miesiac = s.Month AND Dzien = 1)
+MERGE StatystykaPracownicza t USING agentIds s
+	ON t.Id_Data = (SELECT TOP 1 Id FROM dbo.Data WHERE Rok = s.Year AND Miesiac = MONTH(CAST(s.Month as VARCHAR) + ' 1 2021')  AND Dzien = 1)
 	WHEN NOT MATCHED
 		THEN
 			INSERT(Id_Agenta, Id_Data, Premia, [Ilosc godzin], [Wyplata], [Stawka za godzine])
-			VALUES(agentIds.Id,
-			(SELECT Id FROM dbo.Data WHERE Rok = s.Year AND Miesiac = s.Month AND Dzien = 1)
+			VALUES(s.Id,
+			(SELECT TOP 1 Id FROM dbo.Data WHERE Rok = s.Year AND Miesiac = MONTH(CAST(s.Month as VARCHAR) + ' 1 2021') AND Dzien = 1)
 			,s.Bonus,s.HourCount,s.Salary,s.HourlyRate);
-
 
 drop view agentIds;
 drop table dbo.statTEmp;
-
-

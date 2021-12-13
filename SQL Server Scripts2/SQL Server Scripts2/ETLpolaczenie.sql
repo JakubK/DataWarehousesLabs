512e5ac2e6f3e3440625b4e74ec1a4b5a0d4502a
LukaszKond <potrix3@gmail.com>
2021-12-13 14:08:50 +0100

    small additions to ETL polaczenie

--- SQL Server Scripts2/SQL Server Scripts2/ETLpolaczenie.sql
+++ SQL Server Scripts2/SQL Server Scripts2/ETLpolaczenie.sql
@@ -43,3 +43,35 @@ BULK INSERT PolaczenieTemp
 		ROWTERMINATOR = '\n',
 		TABLOCK
 	)
+
+
+	
+If (object_id('dbo.statTemp') is not null) DROP TABLE dbo.statTemp;
+CREATE TABLE statTemp(Year int,[Month] TEXT,AgentId int ,FirstName TEXT,LastName TEXT,DepartmentId int,HourlyRate decimal,Bonus decimal,HourCount int,Salary decimal);
+GO
+--DECLARE @PathToFile varchar(200) = (SELECT TOP(1) PathToFile FROM TmpPaths WHERE name ='excel');
+--PRINT @PathToFile;
+
+BULK INSERT dbo.statTemp
+	FROM 'C:\dev\HDLab\DataWarehousesLabs\DataGenerator\t0_excel.csv'
+	WITH
+	(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = '|',
+		ROWTERMINATOR = '\n',
+		TABLOCK
+	)
+
+	If (object_id('dbo.KlientTemp') is not null) DROP TABLE dbo.KlientTemp;
+CREATE TABLE KlientTemp([Numer Telefonu] varchar(100));
+GO
+
+BULK INSERT KlientTemp
+	FROM 'C:\dev\HDLab\DataWarehousesLabs\DataGenerator\t0_clients.csv'
+	WITH
+	(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = '|',
+		ROWTERMINATOR = '\n',
+		TABLOCK
+	)

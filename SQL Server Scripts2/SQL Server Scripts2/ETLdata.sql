d69a9e3e7f1ca22a5ae5314b065166882ad7cb24
Lukasz <you@example.com>
2021-12-13 11:59:55 +0100

    ETL for data is finished

--- SQL Server Scripts2/SQL Server Scripts2/ETLdata.sql
+++ SQL Server Scripts2/SQL Server Scripts2/ETLdata.sql
@@ -1,27 +1,33 @@
 use CALLCENTER;
-DECLARE @YearInProcess int = 2020;
+
+DECLARE @LowerYearBound int = 1960;
+DECLARE @UpperYearBound int = 2040;
+
+DECLARE @YearInProcess int = @LowerYearBound;
 DECLARE @MonthInProcess int = 1;
 DECLARE @DayInProcess int = 1;
-
-DECLARE @UpperYearBound int = 2030;
-
 
 SET LANGUAGE Polish
 
 WHILE @YearInProcess <= @UpperYearBound
 	BEGIN
+		PRINT @YearInProcess;
 		WHILE @MonthInProcess <= 12
 			BEGIN
 				DECLARE @tmpDate datetime = datefromparts(@YearInProcess, @MonthInProcess, 1)
 				WHILE @DayInProcess <= day(eomonth (@tmpDate))
 					BEGIN
 						INSERT INTO Data(Rok, Miesiac, [Nazwa Miesiaca], Dzien)
-						VALUES(@YearInProcess, @MonthInProcess, DateName( month , DateAdd( month , @MonthInProcess , -1 )), @DayInProcess)
-						SET @DayInProcess = @DayInProcess + 1
+						VALUES(@YearInProcess, @MonthInProcess, DateName( month , DateAdd( month , @MonthInProcess , -1 )), @DayInProcess);
+
+						SET @DayInProcess = @DayInProcess + 1;
 					END
-				SET @MonthInProcess = @MonthInProcess + 1
-				SET @DayInProcess = 1
+
+			SET @MonthInProcess = @MonthInProcess + 1;
+			SET @DayInProcess = 1;
 			END
-		SET @YearInProcess = @YearInProcess + 1
+
+		SET @MonthInProcess = 1;
+		SET @YearInProcess = @YearInProcess + 1;
 	END
 GO

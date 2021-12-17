3e7ebbdd9570f28df87e78c3ec2d8ba689261060
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-12-17 12:47:05 +0100

    Convert call result type to int

--- DataGenerator/Models/PhoneCall.cs
+++ DataGenerator/Models/PhoneCall.cs
@@ -6,7 +6,7 @@ namespace DataGenerator.Models
     {
         public int Id { get; set; }
         public DateTime CallDate { get; set; }
-        public bool Result { get; set; }
+        public int Result { get; set; }
 
         public int ProductId { get; set; }
         public int AgentId { get; set; }
